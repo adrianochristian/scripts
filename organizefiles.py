3d647156d76e3c72af30d7def38b3fc6cddb75d2
Adriano Christian <adriano.christian@atombank.com.br>
2024-05-26 13:48:40 -0300

    feat(cli): adding some text feedback

--- organizefiles.py
+++ organizefiles.py
@@ -4,21 +4,25 @@ from pathlib import Path
 
 home = str(Path.home())
 
-def create_folders():
+def create_folders() -> None:
     if not os.path.exists(home + '/Downloads/Images'):
         os.mkdir(home + '/Downloads/Images')
+        print('Images folder created')
 
     if not os.path.exists(home + '/Downloads/Videos'):
         os.mkdir(home + '/Downloads/Videos')
+        print('Videos folder created')
 
     if not os.path.exists(home + '/Downloads/Audios'):
         os.mkdir(home + '/Downloads/Audios')
+        print('Audios folder created')
 
     if not os.path.exists(home + '/Downloads/Compacted'):
         os.mkdir(home + '/Downloads/Compacted')
+        print('Compacted folder created')
     
 
-def organize():
+def organize() -> None:
     files = os.listdir(os.path.dirname(home + '/Downloads/'))
 
     create_folders()
@@ -26,10 +30,12 @@ def organize():
     for file in files:
         if file.endswith(('.jpg', '.jpeg', '.png', '.gif')):
             os.rename(home + '/Downloads/' + file, home + '/Downloads/Images/' + file)
-        elif file.endswith(('.MP4', '.avi', '.flv', '.wmv', '.mov')):
+        elif file.endswith(('.mp4', '.avi', '.flv', '.wmv', '.mov')):
             os.rename(home + '/Downloads/' + file, home + '/Downloads/Videos/' + file)
         elif file.endswith(('.mp3', '.wav', '.flac', '.aac')):
             os.rename(home + '/Downloads/' + file, home + '/Downloads/Audios/' + file)
         elif file.endswith(('.zip', '.rar', '.tar', '.gz', '.7z')):
             os.rename(home + '/Downloads/' + file, home + '/Downloads/Compacted/' + file)
+    print('Files organized')
+    
 organize()
